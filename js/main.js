86706c20c79648b61de83259b8fdc1e59fae6939
2000dvz <97124288+2000dvz@users.noreply.github.com>
2022-01-07 20:14:56 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+let stepbl1 = document.getElementById("stepbl1");
+let stepbrd1 = document.getElementById("stepbrd1");
+
+
+let stepbl2 = document.getElementById("stepbl2");
+let stepbrd2 = document.getElementById("stepbrd2");
+
+
+let stepbl3 = document.getElementById("stepbl3");
+let stepbrd3 = document.getElementById("stepbrd3");
+
+
+
+
+
+stepbl1.onmouseover = function(){
+	stepbrd1.style.borderBottom = "3px solid white";
+};
+stepbl1.onmouseout = function(){
+	stepbrd1.style.borderBottom = "3px solid #A1C651";
+};
+
+
+
+
+
+stepbl2.onmouseover = function(){
+	stepbrd2.style.borderBottom = "3px solid white";
+};
+stepbl2.onmouseout = function(){
+	stepbrd2.style.borderBottom = "3px solid #A1C651";
+};
+
+
+
+
+stepbl3.onmouseover = function(){
+	stepbrd3.style.borderBottom = "3px solid white";
+};
+stepbl3.onmouseout = function(){
+	stepbrd3.style.borderBottom = "3px solid #A1C651";
+};
+
+
+
+
+
+
+
+
+
